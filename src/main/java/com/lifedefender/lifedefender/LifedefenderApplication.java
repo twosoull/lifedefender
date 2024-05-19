e97b5d02b93583cc40b6ed6301fec87d1979bd67
iyeonghun <twosoull@naver.com>
2024-05-19 22:55:44 +0900

    Initial commit

---
+++ src/main/java/com/lifedefender/lifedefender/LifedefenderApplication.java
@@ -0,0 +1,13 @@
+package com.lifedefender.lifedefender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifedefenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifedefenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifedefender/lifedefender/LifedefenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifedefender.lifedefender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifedefenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
