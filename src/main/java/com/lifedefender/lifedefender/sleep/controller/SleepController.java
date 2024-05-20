394d3a0d00b70a601b4673679ffd06a24975ad4e
iyeonghun <twosoull@naver.com>
2024-05-20 23:27:47 +0900

    rest api 통신 테스트 수정

--- src/main/java/com/lifedefender/lifedefender/sleep/controller/SleepController.java
+++ src/main/java/com/lifedefender/lifedefender/sleep/controller/SleepController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,7 +24,8 @@ public class SleepController {
     }
 
     @PostMapping("/v1/sleep/save")
-    public ResponseEntity<SleepResponseDto> save(SleepRequestDto sleepRequestDto){
+    public ResponseEntity<SleepResponseDto> save(@RequestBody SleepRequestDto sleepRequestDto){
+
         return sleepService.save(sleepRequestDto);
     }
 }
